5bffc8ee7a73f96e2e2ced51a78e5e8fa91c7f30
Priceless001 <100300370+Priceless001@users.noreply.github.com>
2024-04-05 11:29:02 +0100

    Update ourServices.js

--- src/data/ourServices.js
+++ src/data/ourServices.js
@@ -1,29 +1,29 @@
 
-import ImgOne from '../assets/images/our-services/organic-food.webp'
-import ImgTwo from '../assets/images/our-services/express-delivery.webp'
-import ImgThree from '../assets/images/our-services/unforgable-taste.webp'
+// import ImgOne from '../assets/images/our-services/organic-food.webp'
+// import ImgTwo from '../assets/images/our-services/express-delivery.webp'
+// import ImgThree from '../assets/images/our-services/unforgable-taste.webp'
 
 const ourServices = [
     {
         id: 1,
-        img: ImgOne,
+        // img: ImgOne,
         name: 'Organic Food',
         description:
             'Pure and healthy organic food is our lifestyle. The products we consume has impact on out future and we do everything to keep the future healthy',
     },
     {
         id: 2,
-        img: ImgTwo,
+        // img: ImgTwo,
         name: 'Express Delivery',
         description:
             'Choose from a variety of express delivery services depending on your needs. Whether in a hurry to eat or have some plans tomorrow, we have got you covered',
     },
     {
         id: 3,
-        img: ImgThree,
-        name: 'Unforgettable Taste',
+        // img: ImgThree,
+        name: 'Event planning and management',
         description:
-            'Our goal is to provide our customers with excellent service, great taste and unforgettable experiences. This will be a mind-blowing experience for your taste buds',
+            'Our goal is to take off your neck the anxiety and trouble of planning either your corporate or private planning and managing your event',
     },
 ]
 
