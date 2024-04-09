21e9d71556dd0129dda3623aed65cfba6e04ed65
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2024-04-10 00:54:23 +0100

    Update WelcomeSection.js

--- src/routes/landing/WelcomeSection.js
+++ src/routes/landing/WelcomeSection.js
@@ -4,30 +4,12 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import img375 from '../../assets/images/section-one/section-one-375.webp'
 import img700 from '../../assets/images/section-one/section-one-700.webp'
 import img1440 from '../../assets/images/section-one/section-one-1440.webp'
-import PizzaOne from '../../assets/images/welcome-section/farm.png'
-import PizzaTwo from '../../assets/images/welcome-section/event.png'
 import { motion } from "framer-motion";
 
 const WelcomeSection = () => {
   return (
     <article className="welcome-section" >
       <section className="section-2-info flex-container flex-column txt-center pop-font">
-        <motion.img
-          src={PizzaTwo} alt="" className=" pizza-two"
-          initial={{ opacity: 0, translateX: -200 }}
-          whileInView={{
-            opacity: 1, translateX: -100
-          }}
-          transition={{ duration: 5 }}
-        />
-        <motion.img
-          src={PizzaOne} alt="" className=" pizza-one"
-          initial={{ opacity: 0, translateX: 200 }}
-          whileInView={{
-            opacity: 1, translateX: 100
-          }}
-          transition={{ duration: 5 }}
-        />
         <h2 className="txt-white">
           Welcome to <span>MBF NEWDAWN </span> ENTERPRISES
         </h2>
