1b529e417db99aebe44493e4dc629034bb33eeff
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2024-04-11 00:30:53 +0100

    Update MenuGridItem.js

--- src/routes/menu/MenuGridItem.js
+++ src/routes/menu/MenuGridItem.js
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import AddToCartButton from '../cart/AddToCartButton';
+import Attribute from './Attribute';
 import ResetLocation from "../../helpers/ResetLocation";
 
 const MenuGridItem = ({ singleProduct, handleAddProduct, handleRemoveProduct }) => {
+  const [selectedAttributes, setSelectedAttributes] = useState([]);
+  const [targetAttribute, setTargetAttribute] = useState('');
 
- 
+  const handleSelectedAttributes = (attributeId, attributeValue) => {
+    setTargetAttribute(attributeValue);
+    const newSelectedAttribute = { attributeId, attributeValue };
+    setSelectedAttributes(prevAttributes => {
+      const existingAttributeIndex = prevAttributes.findIndex(
+        attribute => attribute.attributeId === newSelectedAttribute.attributeId
+      );
+      if (existingAttributeIndex !== -1) {
+        const updatedAttributes = [...prevAttributes];
+        updatedAttributes[existingAttributeIndex] = { ...newSelectedAttribute };
+        return updatedAttributes;
+      } else {
+        return [...prevAttributes, newSelectedAttribute];
+      }
+    });
+  };
+
 
   return (
     <article className="menu-grid-item flex-container flex-column txt-white">
@@ -14,7 +33,17 @@ const MenuGridItem = ({ singleProduct, handleAddProduct, handleRemoveProduct }) 
       </Link>
       <h3>{singleProduct.ItemName}</h3>
       <p>{singleProduct.ItemIngredients}</p>
-    
+      {singleProduct.attributes.length === 0 ? null :
+        singleProduct.attributes?.map(attribute => (
+          <Attribute
+            key={attribute.id}
+            className="attributes"
+            handleSelectedAttributes={handleSelectedAttributes}
+            attribute={attribute}
+            targetAttribute={targetAttribute}
+          />
+        ))
+      }
       <div className="price">
         {singleProduct.sale === true ?
           <section className="sale-pricing">
@@ -23,11 +52,14 @@ const MenuGridItem = ({ singleProduct, handleAddProduct, handleRemoveProduct }) 
           </section>
           :
           <p className="price-num"><span>₦</span>{singleProduct.ItemPrice}</p>
-        }
+ }
         <AddToCartButton
           handleAddProduct={handleAddProduct}
           handleRemoveProduct={handleRemoveProduct}
           singleProduct={singleProduct}
+          selectedAttributes={selectedAttributes}
+          targetAttribute={targetAttribute}
+          setTargetAttribute={setTargetAttribute}
         />
       </div>
     </article>
@@ -35,4 +67,3 @@ const MenuGridItem = ({ singleProduct, handleAddProduct, handleRemoveProduct }) 
 };
 
 export default MenuGridItem;
-  
