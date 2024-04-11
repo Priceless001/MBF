1c4884ffcb389a72c33c76f1edd9ac68efeb37da
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2024-04-11 11:23:51 +0100

    Update CheckoutForm.js

--- src/routes/checkout/CheckoutForm.js
+++ src/routes/checkout/CheckoutForm.js
@@ -1,22 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { FaShippingFast } from "react-icons/fa";
-import { RiShoppingBagLine } from "react-icons/ri";
 import ResetLocation from "../../helpers/ResetLocation";
 import { Link, useNavigate } from "react-router-dom";
 
-const CheckoutForm = ({ currentUser, totalPayment, productsQuantity, taxes }) => {
-  const [formValue, setFormValue] = useState({
-    fullname: currentUser.fullname, email: currentUser.email, address: currentUser.address, number: currentUser.number, chooseDelivery: "", promoCode: ''
-  });
-  const [submit, setSubmit] = useState(false);
-  const [promoCode, setPromoCode] = useState(false);
-  const [formError, setFormError] = useState({});
-  const navigate = useNavigate();
 
-  const togglePromocode = () => {
-    setPromoCode(!promoCode);
-  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,80 +21,17 @@ const CheckoutForm = ({ currentUser, totalPayment, productsQuantity, taxes }) =>
     const { name, value } = e.target;
     setFormValue({ ...formValue, [name]: value });
   }
-  const validateForm = (value) => {
-    let errors = {}
-    if (!value.chooseDelivery) {
-      errors.chooseDelivery = "Please choose a delivery type"
-    }
-    if (!value.promoCode && promoCode) {
-      errors.promoCode = "Please indicate your promo code"
-    }
-    if (value.promoCode && value.promoCode.length < 5 && promoCode) {
-      errors.promoCode = "Invalid promo code!"
-    }
 
-    return errors;
-  }
 
   return (
     <section className="checkout-personal-information">
       <h3>Personal information <span><Link onClick={ResetLocation} to="/profile">Edit profile</Link></span></h3>
-      <section>
-        <p>{currentUser.fullname}</p>
-        <p>{currentUser.email}</p>
-        {currentUser.address !== null ?
-          <p>{currentUser.address}</p> :
-          <p className="checkout-address">You haven't added address yet<span><Link onClick={ResetLocation} to="/profile">Add address</Link></span></p>}
-        <span className="fullname-error-cpage">{formError.address}</span>
-        {currentUser.number !== null ?
-          <p>{currentUser.number}</p> :
-          <p className="checkout-number">Please add you contact number<span><Link onClick={ResetLocation} to="/profile">Add number</Link></span></p>}
-        <span className="fullname-error-cpage">{formError.number}</span>
-      </section>
+      
       <form onSubmit={handleSubmit}>
         <h3>Delivery details</h3>
-        <label htmlFor="takeaway" className="takeaway-option" name="chooseDelivery">
-          <RiShoppingBagLine />
-          Takeaway
-          <input
-            className="takeaway"
-            type="radio"
-            placeholder="Address"
-            value="takeaway"
-            name="chooseDelivery"
-            onChange={handleValidation}
-          />
-        </label>
-        <label htmlFor="delivery" className="delivery-option" name="chooseDelivery">
-          <FaShippingFast />
-          Delivery
-          <input
-            className="delivery"
-            type="radio"
-            placeholder="Address"
-            value="delivery"
-            name="chooseDelivery"
-            onChange={handleValidation}
-          />
-        </label>
-        <span className="fullname-error-cpage">{formError.chooseDelivery}</span>
-        <section className="promo-code">
-
-          {promoCode === false ? <p onClick={togglePromocode}>I have a promo code!</p> : (
-            <React.Fragment>
-              <p onClick={togglePromocode}>No promo code</p>
-              <input
-                name="promoCode"
-                className=" pop-font"
-                type="text"
-                placeholder="Enter the 5-digit code"
-                onChange={handleValidation}
-                value={formValue.promoCode}
-              />
-            </React.Fragment>
-          )}
-          <span className="fullname-error-cpage">{formError.promoCode}</span>
-        </section>
+        
+        
+        
         <article className="checkout-carttotals">
           {productsQuantity === 0 ? null : (
             <section className="cart-totals">
