a3e722cb315978af6540eb29e0fcaa1ca31dc86a
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2024-04-11 15:30:37 +0100

    Update CheckoutForm.js

--- src/routes/checkout/CheckoutForm.js
+++ src/routes/checkout/CheckoutForm.js
@@ -5,7 +5,12 @@ import { Link } from "react-router-dom";
 const CheckoutForm = ({ totalPayment, productsQuantity, cartItem }) => {
   const handleProceedToPayment = () => {
     // Construct the WhatsApp message link with recipient phone number
-    const whatsappLink = `https://wa.me/08122390396?text=Hello,%20I'm%20interested%20in%20making%20a%20purchase%20of%20${cartItem}.%20My%20total%20payment%20is%20₦${totalPayment}.`;
+    const message = `Hello! I'd like to make a purchase of the following items:\n`;
+    const itemsList = cartItem.map(item => `${CartItem.Itemname}: ${item.quantity} x ${CartItem.Itemprice}`).join('\n');
+    const totalPrice = `Total: ₦${totalPayment}`;
+    const formattedMessage = `${message}${itemsList}\n${totalPrice}`;
+    const encodedMessage = encodeURIComponent(formattedMessage);
+    const whatsappLink = `https://wa.me/08122390396?text=${encodedMessage}`;
     
     // Open the WhatsApp link in a new window
     window.open(whatsappLink, '_blank');
