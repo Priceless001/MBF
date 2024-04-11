d843a4b601eb694cf379a6337ac958c99bf672ed
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2024-04-11 13:16:10 +0100

    Update CheckoutForm.js

--- src/routes/checkout/CheckoutForm.js
+++ src/routes/checkout/CheckoutForm.js
@@ -2,34 +2,8 @@ import React from "react";
 import ResetLocation from "../../helpers/ResetLocation";
 import { Link, useNavigate } from "react-router-dom";
 
-const CheckoutForm = ({ products, totalPayment, productsQuantity, taxes }) => {
+const CheckoutForm = ({ totalPayment, productsQuantity, taxes }) => {
   const navigate = useNavigate();
-
-  const generateWhatsAppMessage = () => {
-    // Construct the message with product details and total payment
-    const message = `Hello! I'd like to make a payment for the following items: \n`;
-    const productList = products.map(product => `${product.name}: ${product.quantity} x ${product.price}`).join('\n');
-    const totalPrice = `Total: $${totalPayment}`;
-    const formattedMessage = `${message}${productList}\n${totalPrice}`;
-
-    // Encode the message for the URL
-    const encodedMessage = encodeURIComponent(formattedMessage);
-
-    // Construct the WhatsApp message link
-    const whatsappLink = `https://wa.me/08122390396?text=${encodedMessage}`;
-
-    // Open the WhatsApp message link in a new window
-    window.open(whatsappLink, '_blank');
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Call the function to generate the WhatsApp message link
-    generateWhatsAppMessage();
-    // Reset location or perform any other necessary actions
-    ResetLocation();
-  }
-  
   return (
     <section className="checkout-personal-information">
       <form onSubmit={handleSubmit}>
