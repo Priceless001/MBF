b386f371366a454bede003a066f7e3bac27e1c1f
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2024-04-11 14:28:28 +0100

    Update CheckoutForm.js

--- src/routes/checkout/CheckoutForm.js
+++ src/routes/checkout/CheckoutForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ResetLocation from "../../helpers/ResetLocation";
+import { Link } from "react-router-dom";
 
 const CheckoutForm = ({ totalPayment, productsQuantity }) => {
   const handleSubmit = (e) => {
@@ -27,11 +28,11 @@ const CheckoutForm = ({ totalPayment, productsQuantity }) => {
           )}
         </article>
         {/* Construct the WhatsApp message link */}
-        <a href={`https://wa.me/08122390396?text=Hello,%20I'm%20interested%20in%20making%20a%20purchase.%20My%20total%20payment%20is%20₦${totalPayment}.`} onClick={ResetLocation}>
+        <Link to={`https://wa.me/08122390396?text=Hello,%20I'm%20interested%20in%20making%20a%20purchase.%20My%20total%20payment%20is%20₦${totalPayment}.`} onClick={ResetLocation}>
           <button type="submit" className="active-button-style">
             Proceed to payment
           </button>
-        </a>
+        </Link>
       </form>
     </section>
   );
