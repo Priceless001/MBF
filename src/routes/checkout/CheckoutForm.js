10af87e944d4c07438bd72d4ff0e66555827b24c
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2024-04-11 11:41:14 +0100

    Update CheckoutForm.js

--- src/routes/checkout/CheckoutForm.js
+++ src/routes/checkout/CheckoutForm.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ResetLocation from "../../helpers/ResetLocation";
 import { Link, useNavigate } from "react-router-dom";
 
-const CheckoutForm = ({ products, totalPayment }) => {
+const CheckoutForm = ({ products, totalPayment, productsQuantity, taxes }) => {
   const navigate = useNavigate();
 
   const generateWhatsAppMessage = () => {
