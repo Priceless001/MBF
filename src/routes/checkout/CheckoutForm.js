0ec8a19ecb8cd7d7db99c98af563564e6ee214a8
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2024-04-14 20:14:40 +0100

    Update CheckoutForm.js

--- src/routes/checkout/CheckoutForm.js
+++ src/routes/checkout/CheckoutForm.js
@@ -43,20 +43,6 @@ const CheckoutForm = ({ totalPayment, productsQuantity, cartItems }) => {
 };
 
 export default CheckoutForm;
-import React from "react";
-import ResetLocation from "../../helpers/ResetLocation";
-import { Link } from "react-router-dom";
 
-const CheckoutForm = ({ totalPayment, productsQuantity }) => {
-  const handleProceedToPayment = () => {
-    // Construct the WhatsApp message link with recipient phone number
-    const whatsappLink = `https://wa.me/+2347067903294?text=Hello,%20I'm%20interested%20in%20making%20a%20purchase%20.%20My%20total%20payment%20is%20₦${totalPayment}.`;
-    
-    // Open the WhatsApp link in a new window
-    window.open(whatsappLink, '_blank');
-
-    // Reset location or perform any other necessary actions
-    ResetLocation();
-  }
 
   
