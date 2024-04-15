9b2d994a924fe1a6b8ecbd38753aad90bd01e36c
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2024-04-15 13:42:13 +0100

    Update ContactUsLanding.js

--- src/routes/landing/ContactUsLanding.js
+++ src/routes/landing/ContactUsLanding.js
@@ -10,8 +10,8 @@ const ContactUsLanding = () => {
           <p>Contact us if you have any questions</p>
         </div>
         <div>
-          <h2>506 Roy Alley 80202 </h2>
-          <p>20, Dorcas Elesho street, Ebute ikorudu, Lagos Nigeria</p>
+          <h2>20, Dorcas Elesho street</h2>
+          <p>Ebute ikorudu, Lagos Nigeria</p>
         </div>
         <div>
           <h2>Open Monday-Friday</h2>
